6cdf715b2a6451cba9e07331a3a493b1edd625e3
andiharun29 <andiharunalrasyid14@gmail.com>
2024-10-13 14:08:57 +0700

    Buat kelas data DataClass dengan properti id sebagai Int dan email sebagai String.

---
+++ app/src/main/java/com/example/latihannn/DataClass.kt
@@ -0,0 +1,14 @@
+package com.example.latihannn
+
+// Data class
+// Data class adalah class yang digunakan untuk menyimpan data.
+// Data class menyediakan fungsi untuk meng-override fungsi equels(), hashCode(), dan toString().
+// Data class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan.
+
+
+data class DataClass(
+    val id: Int,
+    var email: String
+)
+
+
