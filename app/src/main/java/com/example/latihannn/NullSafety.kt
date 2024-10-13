11c09383196d98ee861c8d5254b1cc5a82ef35b0
andiharun29 <andiharunalrasyid14@gmail.com>
2024-10-13 15:19:48 +0700

    Tambahkan fungsi strLength yang menerima parameter notNull, cetak hasil pemanggilan fungsi ini dengan variabel neverNull, yang menghasilkan 18.

--- app/src/main/java/com/example/latihannn/NullSafety.kt
+++ app/src/main/java/com/example/latihannn/NullSafety.kt
@@ -23,3 +23,11 @@ fun nullSafety() {
     println(neverNull.length) // 18
     println(nullable?.length) // null
 
+    // notNull doesn't accept null values
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+
+    println(strLength(neverNull)) // 18
+}
+
