93d8ccbdd91a109f2a2a36b835649e3debc1967b
andiharun29 <andiharunalrasyid14@gmail.com>
2024-10-13 15:29:47 +0700

    Tambahkan fungsi main untuk mencetak nilai dari berbagai tipe data dasar seperti Byte, Short, Int, Long, Float, Double, Char, Boolean, dan String.

---
+++ app/src/main/java/com/example/latihannn/TipeData.kt
@@ -0,0 +1,40 @@
+package com.example.latihannn
+
+fun main(){
+
+    //byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    //int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    //long
+    val longValue: Long = 40
+    println("long: $longValue")
+
+    //Float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    // Double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    // Char
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+    // Boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    // String
+    val stringValue: String = "Hello, world!"
+    println("String: $stringValue")
+}
