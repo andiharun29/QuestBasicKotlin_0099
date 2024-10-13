989190be7e6a68ac36ca0040a87ea8a53725e8f9
andiharun29 <andiharunalrasyid14@gmail.com>
2024-10-13 13:51:15 +0700

    Tambahkan fungsi ConditionalStatement

---
+++ app/src/main/java/com/example/latihannn/ConditionalExpression.kt
@@ -0,0 +1,8 @@
+package com.example.latihannn
+
+//conditional Expression
+//conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+//conditional expression menggunakan if-else, when, and try-catch
+fun ConditionalStatement() {
+    println("=== Conditional Expression ====")
+
