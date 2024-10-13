d96f4c365a194f60cb94fe274fc2ce3f6d6e9617
andiharun29 <andiharunalrasyid14@gmail.com>
2024-10-13 13:52:46 +0700

    Tambahkan pernyataan if-else untuk memeriksa apakah angka 10 adalah bilangan positif atau negatif dan cetak hasilnya.

--- app/src/main/java/com/example/latihannn/ConditionalExpression.kt
+++ app/src/main/java/com/example/latihannn/ConditionalExpression.kt
@@ -6,3 +6,11 @@ package com.example.latihannn
 fun ConditionalStatement() {
     println("=== Conditional Expression ====")
 
+    //if-else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
