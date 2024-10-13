8337080a29580b0f9c9041fee9f474ef506ef06c
andiharun29 <andiharunalrasyid14@gmail.com>
2024-10-13 12:19:13 +0700

    membuat isi dari variabel id dan email beserta dengan jika ada perubahan dan menampilkannya

--- app/src/main/java/com/example/latihannn/Class.kt
+++ app/src/main/java/com/example/latihannn/Class.kt
@@ -3,3 +3,16 @@ package com.example.latihannn
 class motor()
 
 class Contact(val id: Int, var email: String)
+
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+    //prints the value of the property: email
+    println(contact.email)
+
+    // updates the value of property: email
+    contact.email = "jane@gmail.com"
+
+    //print the new value of the property: email
+    println(contact.email)
+}
