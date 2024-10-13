52aa6fc1300ba1468919b20c7627720dae111d8e
andiharun29 <andiharunalrasyid14@gmail.com>
2024-10-13 19:35:11 +0700

    Tambahkan fungsi ContohSet yang mencetak set read-only, set mutable, menambahkan dan menghapus elemen dari set mutable, serta membuat set read-only dari set mutable.

--- app/src/main/java/com/example/latihannn/Collection.kt
+++ app/src/main/java/com/example/latihannn/Collection.kt
@@ -33,6 +33,34 @@ fun contohlist(){
     println(shapesLocked)
 }
 
-fun main(){
-    contohlist()
+//set adalah kumpulan data yang tidak dimiliki posisi atau urutan tertentu dan hanya menyimpan data unik(tidak ada duplikasi)
+//set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+//set bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat  di uabh
+//set Read Only menggunakan setOf
+//set mutbale menggunakan mutableSetOf
+
+fun ContohSet(){
+    println()
+    println("=== Set ===")
+
+    //set read-only
+    val readOnlyAbjad = setOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    //set mutable
+    val shape: MutableSet<String> = mutableSetOf("Circle", "Squere", "Triangle")
+    println(shape)
+
+    //menambahkan data ke dalam set mutable
+    shape.add("RectangLe")
+    println(shape)
+
+    //menghapus data dari set mutable
+    shape.remove("circle")
+    println(shape)
+
+    //set Read-Only
+    val shapesLocked:Set<String> = shape
+    println(shapesLocked)
 }
+
