d480bff72d62160b7b941e8d471283aff1874cde
andiharun29 <andiharunalrasyid14@gmail.com>
2024-10-13 15:00:51 +0700

    Tambahkan fungsi nullSafety yang menampilkan variabel neverNull

---
+++ app/src/main/java/com/example/latihannn/NullSafety.kt
@@ -0,0 +1,9 @@
+package com.example.latihannn
+
+fun nullSafety() {
+    // neverNull has String type
+    var neverNull: String = "This can't be null"
+
+    // Throws a compiler error
+    // neverNull = null
+
