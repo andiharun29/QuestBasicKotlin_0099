48bd7b4d08d69ac388090a9ebc6eab0867fd4bdd
andiharun29 <andiharunalrasyid14@gmail.com>
2024-10-13 15:10:39 +0700

    Tambahkan variabel nullable dengan tipe String yang dapat bernilai null

--- app/src/main/java/com/example/latihannn/NullSafety.kt
+++ app/src/main/java/com/example/latihannn/NullSafety.kt
@@ -7,3 +7,8 @@ fun nullSafety() {
     // Throws a compiler error
     // neverNull = null
 
+    // nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+    // This is OK
+    nullable = null
+
